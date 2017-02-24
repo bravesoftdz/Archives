ef0b8ae39afc2e14330a9015b95f0c6c17bffc1c
alezzle <niekras@tut.by>
2017-02-24 15:09:59 +0300

    dev

--- MoneyRuller/db/mr-dev.sql
+++ MoneyRuller/db/mr-dev.sql
@@ -1,6 +1,6 @@
 ﻿# Host: 127.0.0.1  (Version 5.5.23)
-# Date: 2017-02-23 15:05:16
-# Generator: MySQL-Front 6.0  (Build 1.36)
+# Date: 2017-02-24 14:53:40
+# Generator: MySQL-Front 6.0  (Build 1.37)
 
 /*!40101 SET NAMES utf8 */;
 
@@ -17,10 +17,10 @@ CREATE TABLE `users` (
   PRIMARY KEY (`Id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "users"
 #
 
-REPLACE INTO `users` VALUES (1,'alezzle','123','niekras@tut.by',1);
+REPLACE INTO `users` VALUES (1,'alezzle','$2a$08$jHZj/wJfcVKlIwr5AvR78euJxYK7Ku5kURNhNx.7.CSIJ3Pq6LEPC','niekras@tut.by',1);
---
+++ MoneyRuller/src/MR/CoreBundle/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MR\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller {
+
+    public function loginAction(Request $request) {
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+                    'last_username' => $lastUsername,
+                    'error' => $error,
+        ));
+        
+    }
+
+}
