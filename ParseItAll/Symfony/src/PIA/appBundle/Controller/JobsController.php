7bb203d7ed70425f8c98fb8e339ff000b333ce00
alezzle <niekras@tut.by>
2017-03-27 16:27:30 +0300

    dev

--- ParseItAll/Symfony/src/PIA/appBundle/Controller/JobsController.php
+++ ParseItAll/Symfony/src/PIA/appBundle/Controller/JobsController.php
@@ -3,6 +3,9 @@
 namespace PIA\appBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use PIA\appBundle\Entity\Jobs;
 
 class JobsController extends Controller {
 
@@ -25,7 +28,18 @@ class JobsController extends Controller {
     }
 
     public function addAction() {
-        return $this->render('PIABundle:Editor:job_edit.html.twig');
+
+        $job = new Jobs();
+
+        $form = $this->createFormBuilder($job)
+                ->add('caption', TextType::class)
+                ->add('zeroLink', TextType::class)
+                ->add('Save', SubmitType::class, array('label' => 'Create Job'))
+                ->getForm();
+
+        return $this->render('PIABundle:Editor:job_edit.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 }
