12abc6dff3e8961fd8a561425eb5e9d9f302ec10
alezzle <niekras@tut.by>
2017-05-06 16:49:18 +0300

    dev

---
+++ ParseItAll/DB/pia-dev.sql
@@ -0,0 +1,23 @@
+﻿# Host: 127.0.0.1  (Version 5.5.23)
+# Date: 2017-05-06 16:48:51
+# Generator: MySQL-Front 6.0  (Build 1.157)
+
+
+#
+# Structure for table "users"
+#
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(25) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `is_admin` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
+REPLACE INTO `users` VALUES (1,'admin','123',1);
